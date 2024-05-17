8d02f2d2ebe1647f3b72990877f0fdb1d7b90f3d
AndreyGOIT <ae.borga@gmail.com>
2024-05-17 15:26:42 +0300

    to be repeated rowCount - rowNumber times

--- basicJavaScript/script.js
+++ basicJavaScript/script.js
@@ -2,11 +2,9 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
-  return name;
-}
-const call = padRow("Andy");
-console.log(call);
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+};
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1));
