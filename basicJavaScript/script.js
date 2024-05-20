c01337ac294580e979b06ddbe7820bf5f00a2375
AndreyGOIT <ae.borga@gmail.com>
2024-05-20 15:36:56 +0300

    while loop

--- basicJavaScript/script.js
+++ basicJavaScript/script.js
@@ -11,6 +11,14 @@ for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
 }
 
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+if (done == count) {};
+}
+
 let result = "";
 
 for (const row of rows) {
