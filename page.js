6131deb05f352fc2d9d83ec42c6bf59ec5f63af5
AndreyGOIT <ae.borga@gmail.com>
2024-06-08 14:01:51 +0300

    add bot.js

---
+++ page.js
@@ -0,0 +1,23 @@
+const day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Понедельник");
+    break;
+  case 2:
+    console.log("Вторник");
+    break;
+  case 3:
+    console.log("Среда");
+    break;
+  default:
+    console.log("День недели не определен");
+}
+
+const number = 7;
+
+if (number > 5) {
+  console.log("Больше пяти");
+} else {
+  console.log("Меньше или равно пяти");
+}
