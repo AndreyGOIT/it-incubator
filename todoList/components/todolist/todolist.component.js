860ee49ebf3db741a2b2f01bfc0139abc6eea31d
AndreyGOIT <ae.borga@gmail.com>
2024-06-12 13:35:12 +0300

    initial render

--- todoList/components/todolist/todolist.component.js
+++ todoList/components/todolist/todolist.component.js
@@ -10,12 +10,12 @@ export function Todolist(data) {
     return container;
 };
 
-
 function Header(title) {
     const container = document.createElement('h1');
     container.append(title);
     return container;
 }
+
 function TasksList(tasks) {
     const container = document.createElement('ol');
 
@@ -24,5 +24,19 @@ function TasksList(tasks) {
         const taskElement = Task(task);
         container.append(taskElement);
     }
+    return container;
+}
+
+function Task(task) {
+    const container = document.createElement('li');
+    container.append(task.title);
+    return container;
+}
+
+function ButtonsPanel() {
+    const container = document.createElement('div');
+    const addButtonElement = document.createElement('button');
+    addButtonElement.append('+ Add');
+    container.append(addButtonElement);
     return container;
 }
