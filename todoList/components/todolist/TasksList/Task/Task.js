97b0e4085c6fc32437848ca2a11398fcab946390
AndreyGOIT <ae.borga@gmail.com>
2024-06-12 17:27:37 +0300

    DeleteButton func

--- todoList/components/todolist/TasksList/Task/Task.js
+++ todoList/components/todolist/TasksList/Task/Task.js
@@ -4,12 +4,18 @@ export function Task(task) {
     const container = document.createElement('li');
     container.append(task.title);
 
-    const deleteButtonElement = document.createElement('button');
-    deleteButtonElement.append('❌');
-    deleteButtonElement.addEventListener('click', () => {
-        deleteTask(task.id)
-    });
+    const deleteButtonElement = DeleteButton(task.id);
     container.append(deleteButtonElement);
     
     return container;
+}
+
+function DeleteButton(taskId) {
+    const deleteButtonElement = document.createElement('button');
+    deleteButtonElement.append('❌');
+
+    deleteButtonElement.addEventListener('click', () => {
+        deleteTask(taskId)
+    });
+    return deleteButtonElement;
 }
