82172bc2b8890cbd784ac7bce098ed271c2877a4
AndreyGOIT <ae.borga@gmail.com>
2024-06-13 13:21:57 +0300

    open AddNewTaskDialog

--- todoList/components/todolist/AddNewTaskDialog/AddNewTaskDialog.component.js
+++ todoList/components/todolist/AddNewTaskDialog/AddNewTaskDialog.component.js
@@ -1,3 +1,5 @@
+import { data } from "../../../data/data.js";
+
 export function AddNewTaskDialog() {
     const container = document.createElement('dialog');
     const inputElement = document.createElement('input');
@@ -5,6 +7,10 @@ export function AddNewTaskDialog() {
     saveButtonElement.append('Save');
     const cancelButtonElement = document.createElement('button');
     cancelButtonElement.append('Cancel');
+
+    if (data.todolist.isAddTaskDialogOpen) {
+        container.open = true;
+    }
 
     container.append(inputElement, saveButtonElement, cancelButtonElement);
 
--- todoList/components/todolist/todolist.component.js
+++ todoList/components/todolist/todolist.component.js
@@ -1,6 +1,7 @@
 import { ButtonsPanel } from "./ButtonsPanel/ButtonsPanel.js";
 import { Header } from "./Header/Header.js";
 import { TasksList } from "./TasksList/TasksList.js";
+import { AddNewTaskDialog } from "./AddNewTaskDialog/AddNewTaskDialog.component.js";
 
 export function Todolist(data) {
     const container = document.createElement('div');
@@ -8,11 +9,13 @@ export function Todolist(data) {
     const headerElement = Header(data.title);
     const tasksListElement = TasksList(data.tasks);
     const buttonsElement = ButtonsPanel();
+    const dialogElement = AddNewTaskDialog();
 
     container.append(
         headerElement, 
         tasksListElement, 
-        buttonsElement
+        buttonsElement,
+        dialogElement
     );
 
     return container;
