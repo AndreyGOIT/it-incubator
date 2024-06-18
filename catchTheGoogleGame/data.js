8aaad955cb789ff49ffc5500a0120c25b87c8451
AndreyGOIT <ae.borga@gmail.com>
2024-06-18 10:28:54 +0300

    add data

---
+++ catchTheGoogleGame/data.js
@@ -0,0 +1,32 @@
+export const data = {
+    difficultyLevels: [
+        {
+            gridSize: 4,
+            pointsToWin: 20,
+            pointsToLose: 5,
+        },
+        {
+            gridSize: 5,
+            pointsToWin: 40,
+            pointsToLose: 10,
+        },
+        {
+            gridSize: 6,
+            pointsToWin: 50,
+            pointsToLose: 15,
+        },
+        {
+            gridSize: 7,
+            pointsToWin: 60,
+            pointsToLose: 20,
+        },
+        {
+            gridSize: 8,
+            pointsToWin: 80,
+            pointsToLose: 25,
+        }
+    ],
+    isSoundOn: true,
+    catchedGames: 0,
+    missedGames: 0,
+}
---
+++ catchTheGoogleGame/renderUi.js
@@ -0,0 +1,6 @@
+export function renderUi() {
+    const root = document.querySelector('#root');
+    root.innerHTML = `интерфейс пользователя UI`;
+
+    return root;
+}
