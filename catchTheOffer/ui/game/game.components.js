864422a860147c163edb2f2e3f4d79d0108cacab
AndreyGOIT <ae.borga@gmail.com>
2024-06-30 12:24:32 +0300

    append components to Game

--- catchTheOffer/ui/game/game.components.js
+++ catchTheOffer/ui/game/game.components.js
@@ -1,5 +1,18 @@
+import { Settings } from "./settings/settings.component.js";
+import { Scores } from "./scores/scores.component.js";
+import { Grid } from "./grid/grid.component.js";
+
 export function Game() {
     const containerEl = document.createElement('div');
-    containerEl.append('demo');
+
+    const settingsEl = Settings();
+    containerEl.append(settingsEl);
+
+    const scoresEl = Scores();
+    containerEl.append(scoresEl);
+
+    const gridEl = Grid();
+    containerEl.append(gridEl);
+
     return containerEl;
 }
