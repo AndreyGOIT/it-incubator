c10f2f66ac6c48d08439ae155f40806aa5e32a32
AndreyGOIT <ae.borga@gmail.com>
2024-06-30 13:31:09 +0300

    create grid

--- catchTheOffer/ui/game/grid/grid.component.js
+++ catchTheOffer/ui/game/grid/grid.component.js
@@ -1,9 +1,19 @@
 import { data } from "../../../data/game.data.js";
 
 export function Grid() {
-    const containerEl = document.createElement('div');
+    const containerEl = document.createElement('table');
 
-    containerEl.append('Grid will be here');
+    for (let y = 0; y < data.settings.rowsCount; y++) {
+        const rowEl = document.createElement('tr');
+    for (let x = 0; x < data.settings.columnsCount; x++) {
+            const cellEl = document.createElement('td');
+            cellEl.setAttribute('data-x', x);
+            cellEl.setAttribute('data-y', y);
+            cellEl.append(x + ','+ y);
+            rowEl.append(cellEl);
+        }
+        containerEl.append(rowEl);
+    }
 
     return containerEl;
 }
