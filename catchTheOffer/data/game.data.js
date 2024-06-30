7cd195eddebf28bcc5f6e447a31e6465faef032a
AndreyGOIT <ae.borga@gmail.com>
2024-06-30 12:01:54 +0300

    add coords to data

--- catchTheOffer/data/game.data.js
+++ catchTheOffer/data/game.data.js
@@ -2,7 +2,17 @@ export const data = {
     rowsCount: 3,
     columnsCount: 3,
     coords: {
-        x: 0,
-        y: 0
+        current: {
+            x: 0,
+            y: 0
+        },
+        catched: {
+            x: 0,
+            y: 0
+        },
+        missed: {
+            x: 0,
+            y: 0
+        }
     }
 }
