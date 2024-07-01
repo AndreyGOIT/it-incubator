a51427a2bc22b23f23f21acdcb1ce1a9de2cb6a8
AndreyGOIT <ae.borga@gmail.com>
2024-07-01 10:12:29 +0300

    player OK

--- catchTheOffer/sound/player.js
+++ catchTheOffer/sound/player.js
@@ -1,5 +1,13 @@
+import { data, subscribe, OFFER_STATUSES } from "../data/game.data.js";
+
 export function Player() {
     const catchAudio = new Audio();
     catchAudio.src = 'assets/catch.mp3';
-    catchAudio.play();
+    
+    subscribe(()=> {
+        if (data.status === OFFER_STATUSES.catched) {
+            catchAudio.currentTime = 0;
+            catchAudio.play();
+        }
+    })
 }
