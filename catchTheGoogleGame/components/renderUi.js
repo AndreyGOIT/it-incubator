09fc0cf016f499363b3d9887747d2780ca8d1b03
AndreyGOIT <ae.borga@gmail.com>
2024-07-01 11:29:14 +0300

    refactor GoogleGame

--- catchTheGoogleGame/components/renderUi.js
+++
@@ -1,8 +0,0 @@
-export function RenderUi() {
-    const container = document.createElement('div');
-    container.classList.add('container');
-    container.innerHTML = `интерфейс пользователя UI`
-    
-
-    return container;
-}
