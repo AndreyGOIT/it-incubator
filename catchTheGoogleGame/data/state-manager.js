3f1d148cd2ae6d9d6ce9a6f90daa5138cd043921
AndreyGOIT <ae.borga@gmail.com>
2024-07-02 09:46:24 +0300

    lose handling

--- catchTheGoogleGame/data/state-manager.js
+++ catchTheGoogleGame/data/state-manager.js
@@ -3,6 +3,14 @@ const _state = {
     points: {
         miss: 2,
         catch: 3,
+    },
+    settings: {
+        pointsToLose: 5,
+        pointsToWin: 5,
+        gridSize: {
+            width: 4,
+            height: 4,
+        }
     }
 }
 let _observer = () => {};
@@ -19,5 +27,9 @@ export const getPoints = function () {
 
 setInterval(() => {
     _state.points.miss++;
+
+    if (_state.points.miss >= _state.settings.pointsToLose) {
+        alert('You lose');
+    }
     _observer();
 }, 1000);
