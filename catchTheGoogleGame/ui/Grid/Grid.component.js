a5c34133d83456e7dd03220ed19204b6b74892b3
AndreyGOIT <ae.borga@gmail.com>
2024-07-02 15:37:01 +0300

    draw table and style table

--- catchTheGoogleGame/ui/Grid/Grid.component.js
+++ catchTheGoogleGame/ui/Grid/Grid.component.js
@@ -1,12 +1,14 @@
 export function GridComponent() {
-    const element = document.createElement('div');
-    element.append('Here will be grid component');
-    // element.setAttribute('class', 'grid');
-    // for (let i = 0; i < 16; i++) {
-    //     const cell = document.createElement('div');
-    //     cell.setAttribute('class', 'cell');
-    //     cell.setAttribute('data-id', i);
-    //     element.append(cell);
-    // }
+    const element = document.createElement('table');
+    for (let i = 0; i < 5; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 5; j++) {
+            const cell = document.createElement('td');
+            cell.setAttribute('class', 'cell');
+            cell.setAttribute('data-id', i * 5 + j);
+            row.append(cell);
+        }
+        element.append(row);
+    }
     return element;
 }
