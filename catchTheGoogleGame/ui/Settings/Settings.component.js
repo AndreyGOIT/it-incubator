838c01c3c15ee0dab1725495235ac1a98ff2dc9a
AndreyGOIT <ae.borga@gmail.com>
2024-07-03 16:13:26 +0300

    add ItemsElement 3 & 4

--- catchTheGoogleGame/ui/Settings/Settings.component.js
+++ catchTheGoogleGame/ui/Settings/Settings.component.js
@@ -51,6 +51,48 @@ export function SettingsComponent() {
 
     selectElement_2.append(optionElement_2_1, optionElement_2_2, optionElement_2_3, optionElement_2_4, optionElement_2_5);
     topItemsElement_2.append(labelElement_2, selectElement_2)
+    // ItemsElement_3
+    const topItemsElement_3 = document.createElement('div');
+    topItemsElement_3.classList.add('line');
+
+    const labelElement_3 = document.createElement('label');
+    labelElement_3.append('Points to lose');
+
+    const selectElement_3 = document.createElement('select');
+    selectElement_3.name = 'select';
+    selectElement_3.id = '03';
+
+    const optionElement_3_1 = document.createElement('option');
+    optionElement_3_1.append('5pts');
+    const optionElement_3_2 = document.createElement('option');
+    optionElement_3_2.append('10pts');
+    const optionElement_3_3 = document.createElement('option');
+    optionElement_3_3.append('15pts');
+    const optionElement_3_4 = document.createElement('option');
+    optionElement_3_4.append('20pts');
+    const optionElement_3_5 = document.createElement('option');
+    optionElement_3_5.append('25pts');
+
+    selectElement_3.append(optionElement_3_1, optionElement_3_2, optionElement_3_3, optionElement_3_4, optionElement_3_5);
+    topItemsElement_3.append(labelElement_3, selectElement_3)
+    // ItemsElement_4 / toggle slider
+    const topItemsElement_4 = document.createElement('div');
+    topItemsElement_4.classList.add('switch-button');
+
+    const labelElement_4 = document.createElement('label');
+    labelElement_4.append('Sound on');
+
+    const soundToggleBtn = document.createElement('button');
+    soundToggleBtn.classList.add('toggle');
+    soundToggleBtn.addEventListener('click', () => {
+        toggleSound()
+    });
+
+    const spanElement = document.createElement('span');
+    spanElement.classList.add('icon-slider');
+    soundToggleBtn.append(spanElement);
+
+    topItemsElement_4.append(labelElement_4, soundToggleBtn);
 
     const startGameBtnElement = document.createElement('button');
     startGameBtnElement.textContent = 'START GAME';
@@ -58,8 +100,13 @@ export function SettingsComponent() {
         playAgain();
     });
 
-    element.append(topItemsElement_1, topItemsElement_2)
+    element.append(topItemsElement_1, topItemsElement_2, topItemsElement_3, topItemsElement_4)
     // element.append(gridSizeInput, pointsToWinInput, pointsToLoseInput, soundCheckbox, startGameBtnElement);
 
     return element;
+}
+
+function toggleSound() {
+    let toggleButton = document.querySelector('.toggle')
+    toggleButton.classList.toggle('on');
 }
