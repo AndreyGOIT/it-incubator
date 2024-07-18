54a6bf3c9f3ed93679ba1221d657bf7ef03d88c8
AndreyGOIT <ae.borga@gmail.com>
2024-07-18 13:32:14 +0300

    ResultPanelComponent refactoring

--- catchTheGoogleGame_less07/ui/App.component.js
+++ catchTheGoogleGame_less07/ui/App.component.js
@@ -1,73 +1,61 @@
-import {ResultPanelComponent} from './ResultPanel/ResultPanel.component.js';
-import {LoseComponent} from './Lose/Lose.component.js';
-import {GridComponent} from './Grid/Grid.component.js';
-import { getGameStatus, setObserver } from '../data/state-manager.js';
-import { GAME_STATUSES } from '../data/costants.js';
-import { WinComponent } from './Win/Win.component.js';
-import { SettingsComponent } from './Settings/Settings.component.js';
-import { stopVoiceRecognition } from './controls.js';
+import { ResultPanelComponent } from "./ResultPanel/ResultPanel.component.js";
+import { LoseComponent } from "./Lose/Lose.component.js";
+import { GridComponent } from "./Grid/Grid.component.js";
+import { getGameStatus, setObserver } from "../data/state-manager.js";
+import { GAME_STATUSES } from "../data/costants.js";
+import { WinComponent } from "./Win/Win.component.js";
+import { SettingsComponent } from "./Settings/Settings.component.js";
+import { stopVoiceRecognition } from "./controls.js";
 
 export function AppComponent() {
-    const element = document.createElement('div');
-    element.classList.add('container');
+  const element = document.createElement("div");
+  element.classList.add("container");
 
-    const status = getGameStatus();
-    // console.log('Initial Status:', status);
-    setObserver(()=> {
-        const status = getGameStatus();
-        // console.log('Current Status:', status);
-        element.innerHTML = '';
+  const localState = {
+    status: getGameStatus(),
+  };
+  // console.log('Initial Status:', status);
+  setObserver(() => {
+    const currentStatus = getGameStatus();
+    if (currentStatus === localState.status) return;
+    // console.log('Current Status:', currentStatus);
+    localState.status = currentStatus;
 
-        const transitions = {
-            [GAME_STATUSES.WIN]: () => {
-                const winElement = WinComponent();
-                element.append(winElement);
-                stopVoiceRecognition();
-            },
-            [GAME_STATUSES.LOSE]: () => {
-                const loseElement = LoseComponent();
-                element.append(loseElement);
-                stopVoiceRecognition();
-            },
-            [GAME_STATUSES.SETTINGS]: () => {
-                const settingsElement = SettingsComponent();
-                element.append(settingsElement);
-                stopVoiceRecognition();
-            },
-            [GAME_STATUSES.IN_PROGRESS]: () => {
-                const resultPanelElement = ResultPanelComponent();
-                element.append(resultPanelElement);
-                const gridElement = GridComponent();
-                element.append(gridElement);
-            },
-        }
-        transitions[status]();
-    })
+    element.innerHTML = "";
 
-    const transitions = {
-        [GAME_STATUSES.WIN]: () => {
-            const winElement = WinComponent();
-            element.append(winElement);
-            stopVoiceRecognition();
-        },
-        [GAME_STATUSES.LOSE]: () => {
-            const loseElement = LoseComponent();
-            element.append(loseElement);
-            stopVoiceRecognition();
-        },
-        [GAME_STATUSES.SETTINGS]: () => {
-            const settingsElement = SettingsComponent();
-            element.append(settingsElement);
-            stopVoiceRecognition();
-        },
-        [GAME_STATUSES.IN_PROGRESS]: () => {
-            const resultPanelElement = ResultPanelComponent();
-            element.append(resultPanelElement);
-            const gridElement = GridComponent();
-            element.append(gridElement);
-        },
-    }
-    transitions[status]();
+    render(element, localState);
+  });
 
-    return element;
+  render(element, localState);
+
+  return element;
+}
+
+function render(element, localState) {
+  element.innerHTML = "";
+
+  const transitions = {
+    [GAME_STATUSES.WIN]: () => {
+      const winElement = WinComponent();
+      element.append(winElement);
+      stopVoiceRecognition();
+    },
+    [GAME_STATUSES.LOSE]: () => {
+      const loseElement = LoseComponent();
+      element.append(loseElement);
+      stopVoiceRecognition();
+    },
+    [GAME_STATUSES.SETTINGS]: () => {
+      const settingsElement = SettingsComponent();
+      element.append(settingsElement);
+      stopVoiceRecognition();
+    },
+    [GAME_STATUSES.IN_PROGRESS]: () => {
+      const resultPanelElement = ResultPanelComponent();
+      element.append(resultPanelElement);
+      const gridElement = GridComponent();
+      element.append(gridElement);
+    },
+  };
+  transitions[localState.status]();
 }
