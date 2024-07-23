657c707bfb7d64e04292093eee5648945a9195c5
AndreyGOIT <ae.borga@gmail.com>
2024-07-23 15:39:55 +0300

    build defeatMonster functions

--- rolePlayingGame/script.js
+++ rolePlayingGame/script.js
@@ -110,37 +110,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightSlime() {
-  fighting = 0;
-  goFight()
-}
-
-function fightBeast() {
-  fighting = 1;
-  goFight();
-}
-
-function fightDragon() {
-  fighting = 2;
-  goFight();
-}
-
-function goFight() {
-  update(locations[3]);
-  monsterHealth = monsters[fighting].health;
-  monsterStats.style.display = 'block';
-  monsterName.innerText = monsters[fighting].name;
-  monsterHealthText.innerText = monsterHealth;
-}
-
-function attack() {
-  text.innerText = "The " + monsters[fighting].name + " attacks.";
-  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power
-}
-function dodge() {}
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -182,5 +151,55 @@ function sellWeapon() {
     text.innerText = "Don't sell your only weapon!";
   }
 };
+
+function fightSlime() {
+  fighting = 0;
+  goFight()
+}
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = 'block';
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose()
+    } else if (monsterHealth <= 0) {
+      defeatMonster()
+    }
+}
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
+}
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+function lose() {}
+
+
 
 
